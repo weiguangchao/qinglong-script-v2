cbc0239e70e1512969ba951efbac06d1d7001ae5
wggc <3409885610@qq.com>
2025-10-23 13:43:40 +0800

    添加“机场签到”脚本

---
+++ jc_checkin.js
@@ -0,0 +1,93 @@
+/*
+机场签到
+脚本兼容: Node.js
+
+[Script]
+cron "0 8 * * *" tag=机场签到
+*/
+
+const axios = require('axios');
+
+const loginPath = '/auth/login';
+const checkPath = '/user/checkin';
+
+function login(baseURL, email, passwd) {
+  return axios(baseURL + loginPath, {
+    method: 'POST',
+    params: { email, passwd },
+  })
+    .then((response) => {
+      let data = response.data;
+      if (data.ret != 1) {
+        throw {
+          message: data.msg,
+        };
+      }
+
+      let cookie = response.headers['set-cookie'];
+      return cookie;
+    })
+    .catch((e) => {
+      return '登录失败，原因：' + e.message;
+    });
+}
+
+function check(baseURL, cookie) {
+  return axios(baseURL + checkPath, {
+    method: 'POST',
+    headers: {
+      Cookie: cookie,
+    },
+  })
+    .then((response) => {
+      let data = response.data;
+      if (data.ret != 1) {
+        throw {
+          message: data.msg,
+        };
+      }
+
+      sendMessage.push(data.msg);
+    })
+    .catch((e) => {
+      return '签到失败, 原因：' + e.message;
+    });
+}
+
+function getEnv() {
+  let jcckArr = [];
+  let jcck = process.env.jcck;
+
+  if (!jcck) {
+    console.log('定义环境变量jcck=邮箱;;;密码');
+    process.exit(0);
+  }
+
+  if (Array.isArray(jcck)) {
+    jcckArr = jcck;
+  } else if (jcck.indexOf('&') > -1) {
+    jcckArr = jcck.split('&');
+  } else if (jcck.indexOf('\n') > -1) {
+    jcckArr = jcck.split('\n');
+  } else {
+    jcckArr.push(jcck);
+  }
+
+  return jcckArr;
+}
+
+!(async () => {
+  const jcckArr = getEnv();
+
+  for await (jcck of jcckArr) {
+    jc = jcck.split(';;;');
+    let loginResult = await login(jc[0], jc[1], jc[2]);
+    if (typeof loginResult == 'string') {
+      console.log(`机场地址：${jc[0]}, 邮箱：${jc[1]}，${loginResult}\n`);
+      continue;
+    }
+
+    let checkResult = await check(jc[0], loginResult);
+    console.log(`机场地址：${jc[0]}, 邮箱：${jc[1]}，${checkResult}\n`);
+  }
+})();

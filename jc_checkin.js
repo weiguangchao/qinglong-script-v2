256fda426f769bc58a6e94772b2e460867b0f2fa
weiguangchao <3409885610@qq.com>
2026-03-04 14:18:29 +0800

    refactor: improve string handling and error messages in checkin script
    - Use template literals for string interpolation
    - Include more descriptive error messages
    - Remove hardcoded paths and use string interpolation

--- jc_checkin.js
+++ jc_checkin.js
@@ -6,36 +6,33 @@
  * ck格式: 机场地址;;;邮箱;;;密码
  *
  */
-const axios = require("axios");
-const { Logger, getEnv, sleep } = require("./util.js");
+const axios = require('axios');
+const { Logger, getEnv, sleep } = require('./util.js');
 
-const logger = new Logger("机场签到");
-const ckName = "jcck";
-let cookie = "";
-
-const loginPath = "/auth/login";
-const checkPath = "/user/checkin";
+const logger = new Logger('机场签到');
+const ckName = 'jcck';
+let cookie = '';
 
 async function login(baseURL, email, passwd) {
   logger.log(`${baseURL} ${email} 正在登录`);
-  const response = await axios(baseURL + loginPath, {
-    method: "POST",
+  const response = await axios(`${baseURL}/auth/login`, {
+    method: 'POST',
     params: { email, passwd },
   });
 
   const data = response.data;
   if (data.ret != 1) {
-    throw new Error(data.msg);
+    throw new Error(`登录失败: ${data.msg}`);
   }
 
-  cookie = response.headers["set-cookie"];
-  logger.log("cookie: " + cookie);
+  cookie = response.headers['set-cookie'];
+  logger.log(`cookie: ${cookie}`);
 }
 
 async function check(baseURL) {
   logger.log(`${baseURL} 正在签到`);
-  const response = await axios(baseURL + checkPath, {
-    method: "POST",
+  const response = await axios(`${baseURL}/user/checkin`, {
+    method: 'POST',
     headers: {
       Cookie: cookie,
     },
@@ -43,10 +40,10 @@ async function check(baseURL) {
 
   const data = response.data;
   if (data.ret != 1) {
-    throw new Error(data.msg);
+    throw new Error(`签到失败: ${data.msg}`);
   }
 
-  logger.logAll("签到成功: " + data.msg);
+  logger.logAll(`签到成功: ${data.msg}`);
 }
 
 !(async () => {
@@ -54,7 +51,7 @@ async function check(baseURL) {
 
   for (const ck of ckArr) {
     try {
-      const jc = ck.split(";;;");
+      const jc = ck.split(';;;');
       await login(jc[0], jc[1], jc[2]);
       await sleep(1000);
 
